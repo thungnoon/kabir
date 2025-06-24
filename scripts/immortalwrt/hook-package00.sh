77f09be4ece7c319f649585ae047a4f2c4726f91
thungnoon <kongzheng.zhou@gmail.com>
2025-06-25 06:45:36 +0800

    Update hook-package00.sh

--- scripts/immortalwrt/hook-package00.sh
+++ scripts/immortalwrt/hook-package00.sh
@@ -87,7 +87,12 @@ git clone https://github.com/sbwml/kmod_packages_net_coova-chilli customfeeds/pa
 
 # libxcrypt
 mkdir -p customfeeds/packages/libs/libxcrypt
-curl -s https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/main/package/libs/xcrypt/Makefile > customfeeds/packages/libs/libxcrypt/Makefile
+curl -s https://raw.githubusercontent.com/openwrt/openwrt/a461f53bfdff14341bf84ba4221daeedea3f74bb/package/libs/xcrypt/Makefile > customfeeds/packages/libs/libxcrypt/Makefile
+
+# sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' customfeeds/packages/lang/rust/Makefile
+
+# del mqttled
+rm -rf customfeeds/packages/utils/mqttled
 
 # sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' customfeeds/packages/lang/rust/Makefile
 
