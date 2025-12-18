9503555079d489d611ae7bdfe6d8a3660a6d92ae
thungnoon <kongzheng.zhou@gmail.com>
2025-12-18 10:15:39 +0800

    Update hook-package01.sh

--- scripts/immortalwrt/hook-package01.sh
+++ scripts/immortalwrt/hook-package01.sh
@@ -2,12 +2,10 @@
 # Set to local prepare
 
 # rtpengine
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/rtpengine/900-fix-linux-6.12-11.5.1.18.patch > customfeeds/telephony/net/rtpengine/patches/900-fix-linux-6.12-11.5.1.18.patch
+curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/rtpengine/900-fix-linux-6.12-11.5.1.18.patch > customfeeds/telephony/net/rtpengine/patches/900-fix-linux-6.12-11.5.1.18.patch
 
 # routing - batman-adv fix build with linux-6.12
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/batman-adv/901-fix-linux-6.12rc2-builds.patch > customfeeds/routing/batman-adv/patches/901-fix-linux-6.12rc2-builds.patch
+curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/batman-adv/901-fix-linux-6.12rc2-builds.patch > customfeeds/routing/batman-adv/patches/901-fix-linux-6.12rc2-builds.patch
 
-# libxcrypt
-sed -i "/CONFIGURE_ARGS/i\TARGET_CFLAGS += -Wno-error=pedantic\n" package/libs/xcrypt/libxcrypt/Makefile
-
-rm -rf target/linux/generic/hack-6.12/400-mtd-spinand-Support-fmsh.patch
+# del packages
+# rm -rf customfeeds/packages/net/onionshare-cli
