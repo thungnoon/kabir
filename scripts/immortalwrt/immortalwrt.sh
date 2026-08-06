19f26a886ce79bcde43206a13654e0aa4572d258
thungnoon <kongzheng.zhou@gmail.com>
2026-08-07 07:09:02 +0800

    Update immortalwrt.sh

--- scripts/immortalwrt/immortalwrt.sh
+++ scripts/immortalwrt/immortalwrt.sh
@@ -7,7 +7,6 @@ pushd package/community
 git clone --depth=1 https://github.com/xuanranran/openwrt-package openwrt-package
 git clone --depth=1 https://github.com/xuanranran/rely openwrt-rely
 git clone --depth=1 https://github.com/sbwml/wwan-packages wwan-packages
-chmod 755 openwrt-package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
 popd
 
 # Update OpenClash Panel
@@ -29,8 +28,8 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
 
-# 修改开源站地址
-sed -i '54,56d;63d' scripts/projectsmirrors.json
+# 修改开源站地址 (按内容删除国内镜像, 避免行号漂移破坏 JSON)
+sed -i '\#mirror.iscas.ac.cn/kernel.org#d; \#mirrors.ustc.edu.cn/kernel.org#d; \#mirror.nju.edu.cn/kernel.org#d; \#mirrors.ustc.edu.cn/gnome#d; \#mirror.nju.edu.cn/gnome#d' scripts/projectsmirrors.json
 
 sed -i 's/services/network/g' customfeeds/luci/applications/luci-app-upnp/root/usr/share/luci/menu.d/luci-app-upnp.json
 sed -i 's/services/vpn/g' customfeeds/luci/applications/luci-app-frpc/root/usr/share/luci/menu.d/luci-app-frpc.json
@@ -42,13 +41,3 @@ rm -rf target/linux/x86/base-files/etc/board.d/02_network
 rm -rf package/base-files/files/etc/banner
 cp -f $GITHUB_WORKSPACE/data/banner package/base-files/files/etc/banner
 cp -f $GITHUB_WORKSPACE/data/02_network target/linux/x86/base-files/etc/board.d/02_network
-
-# Kernel - LRNG
-echo -e "\n# Kernel - LRNG" >> .config
-echo "CONFIG_KERNEL_LRNG=y" >> .config
-echo "# CONFIG_PACKAGE_urandom-seed is not set" >> .config
-echo "# CONFIG_PACKAGE_urngd is not set" >> .config
-
-# DPDK
-# echo 'CONFIG_PACKAGE_dpdk-tools=y' >> .config
-# echo 'CONFIG_PACKAGE_numactl=y' >> .config
